c4f54b0d4ce302ee4cfb406904c24a9f2a22f424
Josh Karpel <josh.karpel@gmail.com>
2023-11-06 23:12:10 -0600

    cli command

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,7 +1,40 @@
 (ns advent-of-code.core
+  (:require [cli-matic.core :refer [run-cmd]])
   (:gen-class))
 
+(defn get-input [{:keys [day aoc-session]}]
+  (println "get-input" day aoc-session))
+
+(defn solve [{:keys [day]}]
+  (println "solve" day))
+
+(def CONFIGURATION
+  {:command "advent-of-code"
+   :description "My solutions for Advent of Code 2023"
+   :version "0.1.0"
+   :subcommands [{:command "get-input"
+                  :description "Downloads the input file for a given day"
+                  :examples ["First example" "Second example"]
+                  :opts [{:as "The day to retrieve input for"
+                          :option "day"
+                          :short 0
+                          :default :present
+                          :type :int}
+                         {:as "AoC session token"
+                          :option "aoc-session"
+                          :default :present
+                          :env "AOC_SESSION"
+                          :type :string}]
+                  :runs get-input}
+                 {:command "solve"
+                  :description "Runs the solution for a given day"
+                  :opts [{:as "The day to solve"
+                          :option "day"
+                          :short 0
+                          :default :present
+                          :type :int}]
+                  :runs solve}]})
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (run-cmd args CONFIGURATION))
