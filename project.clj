cc20eba08e12c1857405260a6a540f175e202dcd
Josh Karpel <josh.karpel@gmail.com>
2023-11-06 21:45:54 -0600

    Clojure setup (#1)

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject advent-of-code "0.1.0"
+  :description "My Advent of Code 2023 solutions."
+  :url "https://github.com/JoshKarpel/advent-of-code-2023"
+  :dependencies [[org.clojure/clojure "1.10.2"]]
+  :plugins [[dev.weavejester/lein-cljfmt "0.11.2"]]
+  :main ^:skip-aot advent-of-code.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
